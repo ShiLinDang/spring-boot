f5075200d16b58227f2799dd7c8b7f3c36c92515
545056314@qq.com <shi974075295>
2018-03-28 23:00:44 +0800

    多线程

--- spring-boot/src/main/java/com/baidu/controller/UserController.java
+++ spring-boot/src/main/java/com/baidu/controller/UserController.java
@@ -2,7 +2,6 @@ package com.baidu.controller;
 
 import com.baidu.entity.User;
 import com.baidu.service.UserService;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
