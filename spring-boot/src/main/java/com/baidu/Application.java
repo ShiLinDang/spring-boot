d1a334c450337a4e6ab009e482d55197c059f178
545056314@qq.com <shi974075295>
2018-04-03 22:57:17 +0800

    多线程

--- spring-boot/src/main/java/com/baidu/Application.java
+++ spring-boot/src/main/java/com/baidu/Application.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +20,7 @@ import java.util.concurrent.Executors;
 @MapperScan("com.baidu.dao")
 @RestController
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
 	public static void main(String[] args) {
--- spring-boot/src/main/java/com/baidu/config/RedisConfig.java
+++ spring-boot/src/main/java/com/baidu/config/RedisConfig.java
@@ -3,12 +3,15 @@ package com.baidu.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -43,7 +46,6 @@ public class RedisConfig extends CachingConfigurerSupport{
         };
     }
 
-//    @SuppressWarnings("rawtypes")
 //    @Bean
 //    public CacheManager cacheManager(RedisTemplate redisTemplate) {
 //        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
@@ -53,7 +55,7 @@ public class RedisConfig extends CachingConfigurerSupport{
 //    }
 
     @Bean
-    public RedisTemplate<String, String> getRedisTemplate(JedisConnectionFactory factory) {
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
         StringRedisTemplate template = new StringRedisTemplate(factory);
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
