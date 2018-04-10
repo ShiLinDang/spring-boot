1e130644f2b13b1e70d8f814507037bce30004eb
党诗林 <545056314@qq.com>
2018-04-10 14:26:58 +0800

    添加定时任务

---
+++ spring-boot/src/main/java/com/baidu/controller/LimitTimeController.java
@@ -0,0 +1,19 @@
+package com.baidu.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * 定时任务处理
+ * Created by dangsl on 2018/4/10.
+ */
+@Component
+public class LimitTimeController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    @Scheduled(fixedRate = 10000)
+    public void printTime(){
+        logger.info("定时任务，现在时间："+System.currentTimeMillis());
+    }
+}
